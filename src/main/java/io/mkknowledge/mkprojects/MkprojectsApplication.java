583f8888ff0fc4fd090e9e616ad3f8a06e574602
Mkknowledge <kmayur2124@gmail.com>
2021-03-10 22:41:00 +0530

    first commit

---
+++ src/main/java/io/mkknowledge/mkprojects/MkprojectsApplication.java
@@ -0,0 +1,13 @@
+package io.mkknowledge.mkprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkprojectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mkknowledge/mkprojects/MkprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mkknowledge.mkprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
